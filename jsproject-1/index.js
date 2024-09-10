a536f53b92e81ad57b644d56a0a8cca9f57d7cab
Kayden McManaway <kayden-mcmanaway@live.nmit.ac.nz>
2024-09-11 10:58:33 +1200

    End of Class

---
+++ jsproject-1/index.js
@@ -0,0 +1,12 @@
+// Gets module element from HTML and sets it as an immutable variable
+const module = document.getElementById('module')
+// Gets module closing button from HTML and sets it as an immutable variable
+const moduleClose = document.getElementById('module-close')
+// Gets bookmark form from HTML and sets it as an immutable variable
+const bookmarkForm = document.getElementById('bookmark-form')
+// Gets website name element from HTML and sets it as an immutable variable
+const websiteNameEl = document.getElementById('website-name')
+// Gets website url element from HTML and sets it as an immutable variable
+const websiteUrlEl = document.getElementById('website-url')
+// Gets bookmarks container from HTML and sets it as an immutable variable
+const bookmarksContainer = document.getElementById('bookmarks-container')
